f34595edf1a8050e4de7079c7f5f340245efe3da
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-06-12 21:24:48 +0200

    initial commit hello world

---
+++ 001_hello_world/hello_world.cpp
@@ -0,0 +1,9 @@
+// to compile on linux: c++ -o hello hello_world.cpp or g++ -o hello hello_world.cpp
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, world!\n";
+    return 0;
+}
